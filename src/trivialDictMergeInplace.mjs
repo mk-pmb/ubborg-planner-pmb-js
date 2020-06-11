128a9b8e2147a1d5671cbba29d19d950d1860250
mk-pmb <mk-pmb@users.noreply.github.com>
2020-06-11 05:01:44 +0200

    trivialDictMergeInplace: merge dicts deeply

--- src/trivialDictMergeInplace.mjs
+++ src/trivialDictMergeInplace.mjs
@@ -1,30 +1,39 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
+import aMap from 'map-assoc-core';
 import equalPmb from 'equal-pmb';
+import is from 'typechecks-pmb';
 
-function trivialDictMergeInplace(destDict, ...updates) {
-  if (updates.length > 1) {
-    updates.forEach(u => trivialDictMergeInplace(destDict, u));
-    return destDict;
-  }
-  const [updDict] = updates;
-  if (!updDict) { return destDict; }
-  Object.keys(updDict).forEach(function mergeEntries(key) {
-    const updVal = updDict[key];
+
+
+function mergeSubDict(destDict, pathSteps, updDict) {
+  aMap(updDict, function mergeEntries(updVal, key) {
     if (updVal === undefined) { return; }
     const oldVal = destDict[key];
     if (oldVal === undefined) {
       destDict[key] = updVal; // eslint-disable-line no-param-reassign
       return;
     }
+    if (is.dictObj(oldVal) && is.dictObj(updVal)) {
+      const merged = mergeSubDict({ ...oldVal }, [...pathSteps, key], updVal);
+      destDict[key] = merged; // eslint-disable-line no-param-reassign
+      return;
+    }
     try {
       equalPmb.deepStrictEqual(oldVal, updVal);
     } catch (unequal) {
-      unequal.dictKey = key;
+      unequal.dictPath = [...pathSteps, key];
       unequal.name = 'trivialDictMergeError';
       throw unequal;
     }
   });
+  return destDict;
+}
+
+
+function trivialDictMergeInplace(destDict, ...updates) {
+  if (updates.length < 2) { return mergeSubDict(destDict, [], updates[0]); }
+  updates.forEach(trivialDictMergeInplace.bind(null, destDict, []));
   return destDict;
 };
 
