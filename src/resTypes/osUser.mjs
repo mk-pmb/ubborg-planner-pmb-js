a8e145c00fa934810f07a3932741ad70cf56f9df
mk-pmb <mk-pmb@users.noreply.github.com>
2020-06-15 02:24:25 +0200

    v0.0.21: osUser sshAuthKeys

--- src/resTypes/osUser.mjs
+++ src/resTypes/osUser.mjs
@@ -1,11 +1,85 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
 import pProps from 'p-props';
+import is from 'typechecks-pmb';
 
 import spRes from '../resUtil/simplePassiveResource';
 import parseUserGroupsList from '../parseUserGroupsList';
 
 import osUserLogin from './osUserLogin';
+
+
+async function setupSshAuthKeys(res) {
+  const facts = await res.toFactsDict({ acceptPreliminary: true });
+  const loginName = res.id;
+  const { homeDirPath } = facts;
+  let keys = facts.sshAuthKeys;
+  if (!is.str(keys)) {
+    if (!keys) { return; }
+    if (is.dictObj(keys)) {
+      keys = keys.map(([k, v]) => (k && v && `${v} ${k}`));
+    }
+    if (is.ary(keys)) { keys = keys.filter(Boolean).map(k => k + '\n'); }
+  }
+  if (!homeDirPath) {
+    throw new Error('homeDirPath is required to set up SSH authorized_keys!');
+  }
+
+  function homeSubDir(sub, props) {
+    return res.needs('file', {
+      path: homeDirPath + sub,
+      mimeType: 'dir',
+      enforcedOwner: loginName,
+      enforcedGroup: loginName,
+      enforcedModes: 'a=rx,ug+w',
+      ...props,
+    });
+  };
+  await homeSubDir('');
+  await homeSubDir('/.config');
+  await homeSubDir('/.config/ssh');
+  await homeSubDir('/.ssh', { mimeType: 'sym', content: '.config/ssh' });
+  await homeSubDir('/.config/ssh/authorized_keys', {
+    mimeType: 'text/plain',
+    enforcedModes: 'a=,u+rw',
+    content: keys,
+  });
+}
+
+async function hatch(initExtras) {
+  const res = this;
+  const loginName = res.id;
+  const { spec } = initExtras.spawnOpt;
+
+  const osLogin = {
+    ...spec,
+    groups: undefined,
+    homonymousGroupIdNum: undefined,
+    sshAuthKeys: undefined,
+  };
+
+  const homGrIdNum = spec.homonymousGroupIdNum;
+  if (homGrIdNum) {
+    await res.needs('osUserGroup', { grName: loginName, grIdNum: homGrIdNum });
+    if (osLogin.primaryGroupName === undefined) {
+      osLogin.primaryGroupName = loginName;
+    }
+  }
+
+  await res.needs('osUserLogin', osLogin);
+
+  const { groups } = spec;
+  if (groups) {
+    const memberships = parseUserGroupsList(groups);
+    await pProps(memberships, function setMembership(member, grName) {
+      return res.needs('osUserGroupMembership',
+        { loginName, grName, member });
+    });
+  }
+
+  await setupSshAuthKeys(res);
+}
+
 
 const recipe = {
   typeName: 'osUser',
@@ -17,43 +91,22 @@ const recipe = {
     ...osUserLogin.recipe.acceptProps,
     groups: true,
     homonymousGroupIdNum: true,
+    sshAuthKeys: true,
   },
   uniqueIndexProps: [
     ...osUserLogin.recipe.uniqueIndexProps,
   ],
+  promisingApi: {
+    hatch,
+    finalizePlan() { return this.hatchedPr; },
+  },
 };
 
 const spawnCore = spRes.makeSpawner(recipe);
 
 async function planOsUser(spec) {
   const { loginName } = spec;
-  const res = await spawnCore(this, { loginName });
-  const login = {
-    ...spec,
-    groups: undefined,
-    homonymousGroupIdNum: undefined,
-  };
-
-  const homGrIdNum = spec.homonymousGroupIdNum;
-  if (homGrIdNum) {
-    res.needs('osUserGroup', { grName: loginName, grIdNum: homGrIdNum });
-    if (login.primaryGroupName === undefined) {
-      login.primaryGroupName = loginName;
-    }
-  }
-
-  res.needs('osUserLogin', login);
-
-  const { groups } = spec;
-  if (groups) {
-    const memberships = parseUserGroupsList(groups);
-    await pProps(memberships, function setMembership(member, grName) {
-      return res.needs('osUserGroupMembership',
-        { loginName, grName, member });
-    });
-  }
-
-  return res;
+  return spawnCore(this, { loginName }, { spec });
 }
 
 
