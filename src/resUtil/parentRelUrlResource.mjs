beedf630d53e67181b6e2174793aabd72248dfa3
mk-pmb <mk-pmb@users.noreply.github.com>
2020-06-24 00:36:50 +0200

    v0.0.23: Use URLs for bundles

--- src/resUtil/parentRelUrlResource.mjs
+++ src/resUtil/parentRelUrlResource.mjs
@@ -1,52 +1,29 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
-import normJoinPath from 'norm-join-path';
 import mustBe from 'typechecks-pmb/must-be';
+import bundleUrlUtil from 'ubborg-bundleurl-util-pmb';
 
 import spRes from './simplePassiveResource';
 
 
-function findBaseDir(path) {
-  return (path.replace(/\/+[\x00-.0-\uFFFF]*$/, '') || '.');
-}
-
-
 function makeSpawner(recipe) {
   const { typeName, idProps } = recipe;
-  if ((idProps.length !== 1) || (idProps[0] !== 'path')) {
-    const err = ('For "' + typeName + ' to be a parentRelPathResource, '
-      + 'its recipe must have exactly one idProp, called "path".');
+  if ((idProps.length !== 1) || (idProps[0] !== 'url')) {
+    const err = ('For "' + typeName + ' to be a parentRelUrlResource, '
+      + 'its recipe must have exactly one idProp, called "url".');
     throw new Error(err);
   }
 
-  const parentMustBeObj = mustBe('dictObj', typeName + ' parent');
-  const origApi = recipe.promisingApi;
-  const origFin = mustBe.fun("The base type's finalizePlan function",
-    (origApi || false).finalizePlan);
-  const baseSpawner = spRes.makeSpawner({
-    ...recipe,
-    promisingApi: {
-      ...origApi,
-      finalizePlan(...args) {
-        this.customProps.basedir = findBaseDir(this.id);
-        return origFin.apply(this, args);
-      },
-    },
-  });
-
+  const baseSpawner = spRes.makeSpawner(recipe);
   const { normalizeProps } = baseSpawner.typeMeta;
 
   async function spawn(ctx, origSpec, spawnOpt) {
     const normSpec = normalizeProps(origSpec);
-    let path = mustBe.nest(typeName + ' spec', normSpec.path);
-    const parent = ctx.relatedBy;
-    if (parent) {
-      parentMustBeObj(parent);
-      const parentBaseDir = mustBe.nest(typeName + ' parent basedir',
-        parent.customProps.basedir);
-      path = normJoinPath(parentBaseDir, path);
-    }
-    return baseSpawner(ctx, { ...normSpec, path }, spawnOpt);
+    const parent = (mustBe('undef | eeq:false | dictObj',
+      typeName + ' parent')(ctx.relatedBy) || false);
+    const shortAbsUrl = bundleUrlUtil.shorten(bundleUrlUtil.href(parent.id,
+      mustBe.nest(typeName + ' spec', normSpec.url)));
+    return baseSpawner(ctx, { ...normSpec, url: shortAbsUrl }, spawnOpt);
   }
   return spawn;
 }
