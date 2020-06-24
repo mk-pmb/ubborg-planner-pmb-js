f34060942c898b470e5edf2848b6da8f33065235
mk-pmb <mk-pmb@users.noreply.github.com>
2020-06-24 22:47:06 +0200

    v0.0.25: less quotes for plusText

--- src/depsTree/dumpFmt/plusText.mjs
+++ src/depsTree/dumpFmt/plusText.mjs
@@ -1,11 +1,19 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
+import is from 'typechecks-pmb';
 import univeil from 'univeil';
 
 const { jsonify } = univeil;
 
 function nameLine(color, symb, dest, ev) {
   dest.clog(color, ev.ctx.indent, symb + ' ' + ev.resName);
+}
+
+const rxSimpleId = /^[\w\-]+$/s;
+
+function jsonifyUnlessSimpleId(x) {
+  if (is.str(x) && rxSimpleId.test(x)) { return x; }
+  return jsonify(x);
 }
 
 const formatter = {
@@ -21,7 +29,8 @@ const formatter = {
     nameLine('yellow', '+', dest, ev);
     const { subInd } = ev.ctx;
     await ev.mapFactsDict(function printFact(val, key) {
-      dest.clog('teal', subInd, '= ' + jsonify(key) + ': ' + jsonify(val, -1));
+      dest.clog('teal', subInd, '= ' + jsonifyUnlessSimpleId(key)
+        + ': ' + jsonify(val, -1));
     });
     await ev.diveVerbsSeries();
     dest.clog('brown', subInd, '-', ev.resName);
