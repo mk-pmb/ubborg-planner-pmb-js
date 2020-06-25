b6eeb95a364d09acb86dab8729312c6991b0dcab
mk-pmb <mk-pmb@users.noreply.github.com>
2020-06-25 06:23:31 +0200

    v0.0.29: Fix claimStageFacts

--- src/resUtil/bundle/claimStageFacts.mjs
+++ src/resUtil/bundle/claimStageFacts.mjs
@@ -1,12 +1,15 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
+import mightBeResourcePlan from '../mightBeResourcePlan';
+
 async function claimStageFacts(initExtras, makeClaims) {
   const res = initExtras.getRes();
   await res.hatchedPr;
   const facts = await res.toFactsDict();
   const claims = await makeClaims(facts);
   if (!claims) { return; }
-  const stg = initExtras.getLineageContext().currentStage;
+  const stg = initExtras.getLineageContext().parentStage;
+  if (!stg) { throw new Error('Cannot determine parent stage'); }
   await stg.declareFacts(claims);
 }
 
