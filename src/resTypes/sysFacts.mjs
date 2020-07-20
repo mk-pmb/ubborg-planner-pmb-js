aced20fbcca4d32d225a93bb4735a8129bd6dc60
mk-pmb <mk-pmb@users.noreply.github.com>
2020-07-20 18:40:58 +0200

    v0.0.48: Use sysFacts for osVersion

---
+++ src/resTypes/sysFacts.mjs
@@ -0,0 +1,52 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+/* ********************************************************** *\
+ *                                                            *
+ *      There is an npm package to help with handling         *
+ *      sysFacts resources: ubborg-sysfacts-helper-pmb        *
+ *                                                            *
+\* ********************************************************** */
+
+import mergeOpt from 'merge-options';
+
+import spRes from '../resUtil/simplePassiveResource';
+import makeParamPopperImpl from '../resUtil/makeParamPopper';
+import trivialDictMergeInplace from '../trivialDictMergeInplace';
+
+const resPropsProp = 'customProps';
+const dataProp = 'facts';
+
+const recipe = {
+  ...spRes.recipe,
+  typeName: 'sysFacts',
+  idProps: ['topic'],
+  relationVerbs: [],
+  acceptProps: { [dataProp]: true },
+  directApi: {
+    ...spRes.directApi,
+    getFacts() { return this[resPropsProp][dataProp]; },
+    suggestFacts(sug) {
+      if (!sug) { return; }
+      if (sug === true) { return; }
+      const props = this[resPropsProp];
+      props[dataProp] = mergeOpt(sug, props[dataProp]);
+    },
+    declareFacts(facts) {
+      trivialDictMergeInplace(this[resPropsProp][dataProp], facts);
+    },
+    makeParamPopper(opt) {
+      return makeParamPopperImpl(this.typeName + ' facts',
+        this[resPropsProp][dataProp], opt);
+    },
+  },
+};
+
+const baseSpawner = spRes.makeSpawner(recipe);
+
+export default {
+  async plan(...args) {
+    const res = await baseSpawner(this, ...args);
+    await res.hatchedPr;
+    return res;
+  },
+};
