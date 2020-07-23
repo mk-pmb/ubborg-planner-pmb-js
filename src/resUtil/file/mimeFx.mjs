5b4f913753142b25a5615d24893e589666b4ddc9
mk-pmb <mk-pmb@users.noreply.github.com>
2020-07-23 18:13:55 +0200

    mimeFx: lines + utf8_tw

--- src/resUtil/file/mimeFx.mjs
+++ src/resUtil/file/mimeFx.mjs
@@ -19,6 +19,22 @@ const mtFx = {
     };
   },
 
+  lines(spec) {
+    return {
+      mimeType: 'text/plain; charset=utf-8',
+      content: spec.content.map(ln => ln + '\n'),
+    };
+  },
+
+  utf8_tw(spec) {
+    let [, tw, pre] = spec.mimeType.split(/; */);
+    pre = (pre ? ' ' + pre : '');
+    if (tw === undefined) { tw = 2; }
+    tw = ((+tw || 0) > 0 ? ', tab-width: ' + tw : '');
+    const header = pre + '-*- coding: utf-8' + tw + ' -*-';
+    return mtFx.lines({ content: [header].concat(spec.content) });
+  },
+
 };
 
 
