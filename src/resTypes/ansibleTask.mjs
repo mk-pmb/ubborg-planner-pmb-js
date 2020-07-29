d9f09499ade1ca135f47ce7c364a762d5ed235d7
mk-pmb <mk-pmb@users.noreply.github.com>
2020-07-29 03:55:27 +0200

    ansibleTask: support blocks easier

--- src/resTypes/ansibleTask.mjs
+++ src/resTypes/ansibleTask.mjs
@@ -1,18 +1,31 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
+import mustBe from 'typechecks-pmb/must-be';
+
 import relRes from '../resUtil/parentRelUrlResource';
 
 const recipe = {
   ...relRes.recipe,
   typeName: 'ansibleTask',
   acceptProps: {
-    task: true,
+    tasks: true,
+    blockExtras: true,
   },
 };
 
 const baseSpawner = relRes.makeSpawner(recipe);
 
-function plan(spec, opt, ...extra) {
+function plan(origSpec, opt, ...extra) {
+  mustBe.dictObj('spec', origSpec);
+  const spec = { ...origSpec };
+  if (spec.task) {
+    spec.tasks = [].contat(spec.task, spec.tasks).filter(Boolean);
+    delete spec.task;
+  }
+  if (spec.eachTaskDefaults) {
+    spec.tasks = spec.tasks.map(t => ({ ...spec.eachTaskDefaults, ...t }));
+    delete spec.eachTaskDefaults;
+  }
   return baseSpawner(this, spec, { ...opt, ensureParentUrlTrail: '/' }, extra);
 }
 
