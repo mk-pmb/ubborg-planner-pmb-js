b2b7d4cbccce577a60482063755875fcc8de6557
mk-pmb <mk-pmb@users.noreply.github.com>
2020-07-30 18:44:20 +0200

    v0.0.61: Factor out slashable-import-pmb

--- src/slashableImport.mjs
+++ src/slashableImport.mjs
@@ -1,26 +1,9 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
-import pathLib from 'path';
-
+import makeSlim from 'slashable-import-pmb';
 import bunUrls from 'ubborg-bundleurl-util-pmb';
 
-
-async function impDf(spec) { return (await import(spec)).default; }
-
-
-const slim = async function slashableImport(spec) {
-  let origErr;
-  try { return await impDf(spec); } catch (err) { origErr = err; }
-  if (spec.endsWith('/')) {
-    try {
-      return await impDf(pathLib.join(spec, '__main__'));
-    } catch (ignore) { /* ignore */ }
-    try {
-      return await impDf(pathLib.join(spec, pathLib.basename(spec)));
-    } catch (ignore) { /* ignore */ }
-  }
-  throw origErr;
-};
+const slim = makeSlim();
 
 Object.assign(slim, {
 
