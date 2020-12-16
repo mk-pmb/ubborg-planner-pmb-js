fbfe7abb734959178b358e7c1794e7d0638cf156
mk-pmb <mk-pmb@users.noreply.github.com>
2020-12-16 23:33:59 +0100

    file: Support mountpoints

--- src/resUtil/file/mimeAlias.mjs
+++ src/resUtil/file/mimeAlias.mjs
@@ -4,6 +4,7 @@ const mta = {
   blk:  'inode/blockdevice',
   char: 'inode/chardevice',
   dir:  'inode/directory',
+  dirOrMounted: 'inode/directory; or-active-mountpoint',
   fifo: 'inode/fifo',
   sock: 'inode/socket',
   sym:  'inode/symlink',  // only for broken ones. see the "symlink" resType.
