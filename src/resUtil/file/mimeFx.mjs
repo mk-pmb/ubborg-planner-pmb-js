8490724d8fb9a194dc2bf6a7b69738b35a50b0a6
mk-pmb <mk-pmb@users.noreply.github.com>
2021-01-06 06:54:06 +0100

    v0.0.89: file: mimeFx utf8_tw: Support percent-encoded prefix

--- src/resUtil/file/mimeFx.mjs
+++ src/resUtil/file/mimeFx.mjs
@@ -37,7 +37,7 @@ const mtFx = {
 
   utf8_tw(spec) {
     let [, tw, pre] = spec.mimeType.split(/; */);
-    pre = (pre ? pre + ' ' : '');
+    pre = (pre ? decodeURIComponent(pre) + ' ' : '');
     if (tw === undefined) { tw = 2; }
     tw = ((+tw || 0) > 0 ? ', tab-width: ' + tw : '');
     const header = pre + '-*- coding: utf-8' + tw + ' -*-';
