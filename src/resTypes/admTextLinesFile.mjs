d93e0f9673c3ed6f0f6dda725b8d11dcb57fdeff
mk-pmb <mk-pmb@users.noreply.github.com>
2021-06-08 23:23:10 +0200

    v0.0.94: admTextLinesFile

---
+++ src/resTypes/admTextLinesFile.mjs
@@ -0,0 +1,12 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+import preCfg from 'ubborg-restype-util-pmb/src/preconfiguredResType';
+
+import origResType from './file.mjs';
+
+export default preCfg(origResType, {
+  enforcedOwner: 'root',
+  enforcedGroup: 'adm',
+  enforcedModes: 'a-x,a=rX,ug+w', // a-x ensures X only matches directories
+  mimeType: 'lines',
+});
